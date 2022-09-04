4e171e10a2c3fd033e8e74530c9dcbddea3e6cd6
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2022-09-04 13:16:58 +0200

    JS updated

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function displayTemperature(respons) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = respons.data.main.temp;
+}
 
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);

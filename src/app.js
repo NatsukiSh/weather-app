99feb7608d685f299a2ec850fceff7a1c516bee8
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2022-09-10 13:27:39 +0200

    JS updated

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-function displayTemperature(respons) {
+function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = respons.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiKey = "28feed74ca8e6f8450fa975c80fe27e3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
